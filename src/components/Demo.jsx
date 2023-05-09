4aed4f0d7a2c0969627bac53f75496907da404b5
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-09 23:08:39 +0800

    Add function to handleSubmit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,15 +1,30 @@
 import { useState, useEffect } from 'react'
 
 import { copy, linkIcon, loader, tick } from '../assets'
+import { useLazyGetSummaryQuery } from '../services/article'
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: '',
     summary: ''
   })
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
 
   const handleSubmit = async(e) => {
-    
+    e.preventDefault()
+
+    const { data } = await getSummary({
+      articleUrl: article.url
+    })
+
+    if (data?.summary) {
+      const newArticle = {
+        ...article,
+        summary: data.summary
+      }
+
+      setArticle(newArticle)
+    }
   }
 
   return (
@@ -19,7 +34,7 @@ const Demo = () => {
         <form onSubmit={handleSubmit} className="relative flex justify-center items-center">
           <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
 
-          <input type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => {setArticle({ article, url: e.target.value })}} required className="url_input peer" />
+          <input type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer" />
 
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↵</button>
         </form>
