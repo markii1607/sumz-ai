c00100543344eea687e7b0d48d20656f3758c079
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-10 21:31:52 +0800

    Add copy to cliboard function

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,7 @@ const Demo = () => {
     summary: ''
   })
   const [allArticles, setAllArticles] = useState([])
+  const [copied, setCopied] = useState("")
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
 
   useEffect(() => {
@@ -43,6 +44,15 @@ const Demo = () => {
     }
   }
 
+  // click to copy the URL
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+
+    // remove the copied function after 3s
+    setTimeout(() => setCopied(false), 3000)
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
@@ -59,8 +69,8 @@ const Demo = () => {
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.map((item, index) => (
             <div key={`link-${index}`} onClick={() => setArticle(item)} className="link_card">
-              <div className="copy_btn">
-                <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img src={copied === item.url ? tick : copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
               </div>
               <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
             </div>
